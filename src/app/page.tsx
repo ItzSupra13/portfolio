4f242ef03e95448159f3aab14e03b1bc26db2ca9
ItzSupra13 <schakraborty.lmb20@gmail.com>
2026-05-18 11:56:54 +0530

    Changed og image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,49 +16,49 @@ import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip
 export default function Page() {
   return (
     <main className="relative min-h-screen overflow-x-hidden bg-[#0d0d0d] text-white selection:bg-white/10">
-      <GridSection Hero showHashes showPattern>
+      <GridSection Hero showHashes showPattern showCrosshairs>
         <Hero />
       </GridSection>
 
-      <GridSection hideOnMobile>
+      <GridSection hideOnMobile topBorder={false}>
         <About />
       </GridSection>
 
-      <GridSection hideOnMobile showHashes>
+      <GridSection hideOnMobile showHashes topBorder={false} showCrosshairs>
         <Experience />
       </GridSection>
 
-      <GridSection hideOnMobile>
+      <GridSection hideOnMobile topBorder={false}>
         <Work />
       </GridSection>
 
-      <GridSection showHashes hideOnMobile>
+      <GridSection showHashes hideOnMobile topBorder={false} showCrosshairs>
         <Writing />
       </GridSection>
 
-      <GridSection hideOnMobile>
+      <GridSection hideOnMobile topBorder={false}>
         <Tools />
       </GridSection>
 
-      <GridSection showHashes hideOnMobile>
+      <GridSection showHashes hideOnMobile topBorder={false} showCrosshairs>
         <Education />
       </GridSection>
 
-      <GridSection hideOnMobile>
+      <GridSection hideOnMobile topBorder={false}>
         <TechStack />
       </GridSection>
 
-      <GridSection showHashes hideOnMobile>
+      <GridSection showHashes hideOnMobile topBorder={false} showCrosshairs>
         <Personal />
       </GridSection>
 
       {/* CONTACT: Maybe you don't want a bottom border right before the footer */}
-      <GridSection hideOnMobile>
+      <GridSection hideOnMobile topBorder={false}>
         <Contact />
       </GridSection>
 
       {/* FOOTER */}
-      <GridSection showHashes hideOnMobile>
+      <GridSection showHashes hideOnMobile topBorder={false} showCrosshairs>
         <footer className="flex items-center justify-center py-8 text-xs text-[#8b8b8b]">
           © 2026 | Made with <FaHeart className="mx-1 text-orange-700" /> by&nbsp;
           <Tooltip>
